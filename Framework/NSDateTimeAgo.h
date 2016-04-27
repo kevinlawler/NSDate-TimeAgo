64f8886b3dd39a541917c573a8557aa3a62f4b8f
Ben Baron <ben@einsteinx2.com>
2016-04-26 17:13:32 -0700

    Added OSX framework support (works with Carthage)

--- Framework/NSDateTimeAgo.h
+++ Framework/NSDateTimeAgo.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Kevin Lawler. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for NSDate-TimeAgo.
 FOUNDATION_EXPORT double NSDate_TimeAgoVersionNumber;
